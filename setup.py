a5ca35622ecc344abbcd4ac0d666b18093f9fe6a
Roman Orac <orac.roman@gmail.com>
2019-11-06 18:39:33 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cf_text_embeddings',
-    version='0.0.1',
+    version='0.1.0',
     packages=['cf_text_embeddings'],
     include_package_data=True,
     license='MIT License',
