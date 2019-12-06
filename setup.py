90b2b1a531fea8d3f52070325050b97586f4c0e7
Roman Orac <orac.roman@gmail.com>
2019-12-06 22:37:59 +0100

    Change requirements to Pipfile

--- setup.py
+++ setup.py
@@ -3,20 +3,23 @@ import re
 
 from setuptools import setup
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
     # remove multiple spaces because of pypi requirement
     README = re.sub(' +', '', README)
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cf_text_embeddings',
-    version='0.1.6',
+    version='0.1.7',
     packages=['cf_text_embeddings'],
     include_package_data=True,
     license='MIT License',
