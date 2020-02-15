cc90acbe550f35a2b12a71982f12de7dee206640
Roman Orac <orac.roman@gmail.com>
2020-02-15 23:28:01 +0100

    Add new methods for word2vec

--- cf_text_embeddings/library.py
+++ cf_text_embeddings/library.py
@@ -1,10 +1,11 @@
 import transformers
 from Orange.data import Table
 
+from cf_text_embeddings.base.table import orange_data_table
 from cf_text_embeddings.base import io, tokenizers
-from cf_text_embeddings.common import (load_numpy_array, map_checkbox_value,
-                                       map_y, orange_domain, to_float, to_int)
-from cf_text_embeddings.embeddings_model import (
+from cf_text_embeddings.base.common import (load_numpy_array,
+                                            map_checkbox_value, map_y, to_float, to_int)
+from cf_text_embeddings.base.embeddings_model import (
     EmbeddingsModelBert, EmbeddingsModelDoc2Vec, EmbeddingsModelElmo,
     EmbeddingsModelFastText, EmbeddingsModelGloVe, EmbeddingsModelLSI,
     EmbeddingsModelUniversalSentenceEncoder, EmbeddingsModelWord2Vec)
@@ -38,9 +39,9 @@ def cf_text_embeddings_punkt_tokenizer(input_dict):
     return input_dict
 
 
-def cf_text_embeddings_extract_model_name(input_dict, languages):
+def cf_text_embeddings_extract_models_language(input_dict, languages):
     # language selector overrides the language in the widget
-    lang = input_dict['lang_selector'] or input_dict['lang']
+    lang = input_dict.get('lang_selector') or input_dict.get('lang')
     model_name = languages.get(lang)
     if model_name is None:
         raise Exception('Model for %s language is not supported' % lang)
@@ -65,9 +66,18 @@ def cf_text_embeddings_word2vec(input_dict):
         'lv': 'word2vec_lv.wv',
         # http://vectors.nlpl.eu/repository/
         'ee': 'word2vec_ee.wv',
-    }
-    lang, model_name = cf_text_embeddings_extract_model_name(input_dict, languages)
-    return {'embeddings_model': EmbeddingsModelWord2Vec(lang, model_name)}
+        'test': 'word2vec_test_model.bin'}
+    lang, model_name = cf_text_embeddings_extract_models_language(input_dict, languages)
+    embeddings_model = EmbeddingsModelWord2Vec(lang, model_name)
+
+    aggregation_method = input_dict.get('aggregation_method')
+    weighting_method = input_dict.get('weighting_method')
+    texts = input_dict['texts']
+    embeddings = embeddings_model.apply(texts, aggregation_method, weighting_method)
+
+    labels = input_dict['labels']
+    dataset = orange_data_table(embeddings, labels)
+    return {'dataset': dataset}
 
 
 def cf_text_embeddings_glove(input_dict):
@@ -79,7 +89,7 @@ def cf_text_embeddings_glove(input_dict):
         # https://deepset.ai/german-word-embeddings
         'de': 'glove_de.wv',
     }
-    lang, model_name = cf_text_embeddings_extract_model_name(input_dict, languages)
+    lang, model_name = cf_text_embeddings_extract_models_language(input_dict, languages)
     return {'embeddings_model': EmbeddingsModelGloVe(lang, model_name)}
 
 
@@ -104,7 +114,7 @@ def cf_text_embeddings_fasttext(input_dict):
         # https://github.com/facebookresearch/MUSE
         'hr': 'wiki.multi.hr.wv',
     }
-    lang, model_name = cf_text_embeddings_extract_model_name(input_dict, languages)
+    lang, model_name = cf_text_embeddings_extract_models_language(input_dict, languages)
     return {'embeddings_model': EmbeddingsModelFastText(lang, model_name)}
 
 
@@ -128,16 +138,16 @@ def cf_text_embeddings_bert(_):
                             default_token_annotation='TextBlock')
     }
 
-    def cf_text_embeddings_universal_sentence_encoder(input_dict):
-        languages = {
-            # https://tfhub.dev/google/universal-sentence-encoder/2
-            'en': 'universal_sentence_encoder_english',
-            # https://tfhub.dev/google/universal-sentence-encoder-xling/en-de/1
-            'de': 'universal_sentence_encoder_german',
-            # https://tfhub.dev/google/universal-sentence-encoder-xling/en-es/1
-            'es': 'universal_sentence_encoder_spanish',
-        }
-        lang, model_name = cf_text_embeddings_extract_model_name(input_dict, languages)
+def cf_text_embeddings_universal_sentence_encoder(input_dict):
+    languages = {
+        # https://tfhub.dev/google/universal-sentence-encoder/2
+        'en': 'universal_sentence_encoder_english',
+        # https://tfhub.dev/google/universal-sentence-encoder-xling/en-de/1
+        'de': 'universal_sentence_encoder_german',
+        # https://tfhub.dev/google/universal-sentence-encoder-xling/en-es/1
+        'es': 'universal_sentence_encoder_spanish',
+    }
+    lang, model_name = cf_text_embeddings_extract_models_language(input_dict, languages)
 
     return {
         'embeddings_model':
@@ -145,53 +155,47 @@ def cf_text_embeddings_bert(_):
                                                 default_token_annotation='Sentence')
     }
 
-    def cf_text_embeddings_doc2vec(input_dict):
-        languages = {
-            # https://github.com/jhlau/doc2vec
-            'en': 'doc2vec.bin',
-        }
-        lang, model_name = cf_text_embeddings_extract_model_name(input_dict, languages)
+def cf_text_embeddings_doc2vec(input_dict):
+    languages = {
+        # https://github.com/jhlau/doc2vec
+        'en': 'doc2vec.bin',
+    }
+    lang, model_name = cf_text_embeddings_extract_models_language(input_dict, languages)
 
     return {
         'embeddings_model':
         EmbeddingsModelDoc2Vec(lang, model_name, default_token_annotation='TextBlock')
     }
 
-    def cf_text_embeddings_elmo(input_dict):
-        languages = {
-            # http://vectors.nlpl.eu/repository/#
-            'en': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'sl': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'es': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'ru': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'hr': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'ee': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'lv': 'elmo',
-            # http://vectors.nlpl.eu/repository/#
-            'de': 'elmo',
-        }
-        lang, model_name = cf_text_embeddings_extract_model_name(input_dict, languages)
+def cf_text_embeddings_elmo(input_dict):
+    languages = {
+        # http://vectors.nlpl.eu/repository/#
+        'en': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'sl': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'es': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'ru': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'hr': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'ee': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'lv': 'elmo',
+        # http://vectors.nlpl.eu/repository/#
+        'de': 'elmo',
+    }
+    lang, model_name = cf_text_embeddings_extract_models_language(input_dict, languages)
 
     return {'embeddings_model': EmbeddingsModelElmo(lang, model_name)}
 
 
 def cf_text_embeddings_embeddings_hub(input_dict):
-    embeddings_model = input_dict['embeddings_model']
-
-    default_token_annotation = embeddings_model.default_token_annotation
-    token_annotation = input_dict['token_annotation'] or default_token_annotation or 'Token'
     aggregation_method = input_dict['aggregation_method']
     weighting_method = input_dict['weighting_method']
-    adc = input_dict['adc']
-    documents = adc.documents
-    bow_dataset = embeddings_model.apply(documents, token_annotation, aggregation_method,
-                                         weighting_method)
+    texts, labels = input_dict['texts']
+    bow_dataset = embeddings_model.apply(dc, aggregation_method, weighting_method)
     return {'bow_dataset': bow_dataset}
 
 
