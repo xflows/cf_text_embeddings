04d3682c1ff253032807dcf13225c752936459f2
Roman Orac <orac.roman@gmail.com>
2020-02-15 21:15:47 +0100

    Add test library

---
+++ cf_text_embeddings/test/test_library.py
@@ -0,0 +1,37 @@
+import unittest
+from os import path
+
+from cf_text_embeddings.library import (cf_text_embeddings_parse_csv,
+                                        cf_text_embeddings_tok_tok_tokenizer)
+
+ROOT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+
+
+class TestLibrary(unittest.TestCase):
+    def test_parse_csv(self):
+        input_dict = {
+            'input': path.join(ROOT_DIR, 'corpus/basic.tsv'),
+            'text_index': 2,
+            'label_index': 3,
+            'skip_header': 'true',
+        }
+        output_dict = cf_text_embeddings_parse_csv(input_dict)
+        self.assertEqual(True, 'texts' in output_dict)
+        self.assertEqual(True, 'labels' in output_dict)
+        self.assertEqual(True, len(output_dict) == 2)
+        self.assertEqual(True, len(output_dict['texts']) == 4)
+        self.assertEqual(True, len(output_dict['labels']) == 4)
+
+    def test_toktok_tokenizer(self):
+        input_dict = {'texts': ['this is doc1'], 'labels': [0]}
+        output_dict = cf_text_embeddings_tok_tok_tokenizer(input_dict)
+        self.assertEqual(True, 'texts' in output_dict)
+        self.assertEqual(True, 'labels' in output_dict)
+        self.assertEqual(True, len(output_dict) == 2)
+        self.assertEqual(True, len(output_dict['texts']) == 1)
+        self.assertEqual(True, len(output_dict['texts'][0]) == 3)
+        self.assertEqual(True, len(output_dict['labels']) == 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
