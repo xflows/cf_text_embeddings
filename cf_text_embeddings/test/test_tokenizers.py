f6d3a912d8ebd73f472abff0c1c3669286a63976
Roman Orac <orac.roman@gmail.com>
2020-02-15 21:14:34 +0100

    Add test tokenizers

---
+++ cf_text_embeddings/test/test_tokenizers.py
@@ -0,0 +1,24 @@
+import unittest
+
+from cf_text_embeddings.base.tokenizers import (punkt_tokenizer,
+                                                toktok_tokenizer)
+
+
+class TestTokenizers(unittest.TestCase):
+    def test_toktok_tokenizer(self):
+        texts = ['this is some text', 'this is some other text']
+        texts_tokenized = toktok_tokenizer(texts)
+        for i, text in enumerate(texts):
+            tokens = text.split(' ')
+            for j, token in enumerate(tokens):
+                self.assertEqual(token, texts_tokenized[i][j])
+
+    def test_punkt_tokenizer(self):
+        texts = ['this is some text', 'this is some other text']
+        texts_tokenized = punkt_tokenizer(texts, language='english')
+        for i, text in enumerate(texts):
+            self.assertEqual([text], texts_tokenized[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
