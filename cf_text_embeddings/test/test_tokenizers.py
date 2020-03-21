fd18454cb2f035ba348d617037a6f47a3f7147d1
Roman Orac <orac.roman@gmail.com>
2020-03-21 15:22:19 +0100

    Test tokenizers with empty inputs

--- cf_text_embeddings/test/test_tokenizers.py
+++ cf_text_embeddings/test/test_tokenizers.py
@@ -12,12 +12,23 @@ class TestTokenizers(unittest.TestCase):
             for j, token in enumerate(tokens):
                 self.assertEqual(token, texts_tokenized[i][j])
 
+    def test_toktok_tokenizer_empty_input(self):
+        texts = ['']
+        texts_tokenized = toktok_tokenizer(texts)
+        self.assertEqual(1, len(texts_tokenized))
+        self.assertEqual(0, len(texts_tokenized[0]))
+
     def test_punkt_tokenizer(self):
         texts = ['this is some text', 'this is some other text']
         texts_tokenized = punkt_tokenizer(texts, language='english')
         for i, text in enumerate(texts):
             self.assertEqual([text], texts_tokenized[i])
 
+    def test_punkt_tokenizer_empty_input(self):
+        texts = ['']
+        texts_tokenized = punkt_tokenizer(texts, language='english')
+        self.assertEqual(1, len(texts_tokenized))
+        self.assertEqual(0, len(texts_tokenized[0]))
 
 if __name__ == '__main__':
     unittest.main()
