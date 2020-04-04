cca02fadda22fef037dc27ca06b2c9a406df597c
Roman Orac <orac.roman@gmail.com>
2020-04-04 15:53:29 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cf_text_embeddings',
-    version='0.1.12',
+    version='0.1.13',
     packages=['cf_text_embeddings'],
     include_package_data=True,
     license='MIT License',
@@ -26,7 +26,6 @@ setup(
     author='Roman Orac',
     author_email='orac.roman@gmail.com',
     install_requires=requirements,
-    setup_requires=['nltk'],
 )
 
 
